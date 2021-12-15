b4d4bb39835cc8e22e32b804783ba5b9a7db9127
umarnurmatov <umarkhonnur12@gmail.com>
2021-12-15 08:21:58 +0300

    Создан модуль Interlayer
    Объединяет модули FaceRec и Spreadsheet

---
+++ google_sheets_api/interlayer.py
@@ -0,0 +1,86 @@
+import pandas as pd
+from sheets_api import Spreadsheet
+import urllib.request
+
+# Название файла с локальной таблицей
+LOCAL_MARK_DATA = "local_mdata.xlsx"
+
+class Interlayer:
+    '''
+    Промежуточный модуль между CV и GoogleSheets   
+    
+    Основная задача - проверить наличие Интернета, и при его отсутствии сохранить
+    данные локально. Если Интернет есть, выгружает все локальные данные в сеть.
+    '''
+    
+    def __is_connected_to_internet__(self, host='http://google.com'):
+        '''
+        Проверка Интернет-соединения
+
+        host : Сайт, на который пытаемся зайти, string. Дефолт 'http://google.com'.
+            - true - если Интернет-соединение есть
+            - false - нет
+        '''
+        try:
+            urllib.request.urlopen(host) 
+            return True
+        except:
+            return False
+        
+    spreadsheet_init_flag = False
+    def __connect_to_google_sheets__(self):
+        '''
+        Попытка подключения к GoogleSheets и создания service-объекта
+
+        При наличии Интернета создает объект класса Spreadsheet и инициализирует его; 
+        опускает флаг
+        
+        При отсутствии Интернета оставляет флаг поднятым; при следующей попытке
+        выставить оценку будет осуществлена еще одна попытка
+        '''
+        if self.__is_connected_to_internet__() and not self.spreadsheet_init_flag:
+            self.spreadsheet = Spreadsheet()
+            self.spreadsheet_init_flag = True
+            
+    def __init__(self):
+        self.__connect_to_google_sheets__()
+        
+        # Таблица с локальными данными об оценках
+        # +--------------+----+
+        # | date         | ID | 
+        # +--------------+----+
+        # |  <ДД.ММ.ГГ>  | 0  |
+        # +--------------+----+
+        # |  <ДД.ММ.ГГ>  | 1  |
+        # +--------------+----+
+        self.local_mdata = pd.read_excel(LOCAL_MARK_DATA)
+    
+
+    def put_mark(self, date, ID):
+        # Соединение с GoogleSheets для работы с интернет-таблицей
+        self.__connect_to_google_sheets__()
+        
+        if self.__is_connected_to_internet__():
+            # Считываем локальные данные об оценках
+            for i in range(len(self.local_mdata['date'])):
+                mdata = [self.local_mdata['date'][i], self.local_mdata['ID'][i]]
+                self.spreadsheet.put_mark(mdata)
+            # Очищаем локальные данные
+            self.local_mdata = pd.DataFrame({'date':[], 'ID':[]})
+            
+            # Выставляем новую оценку
+            mdata = [date, ID]
+            self.spreadsheet.put_mark(mdata)
+        else:
+            # Если Интернета нет, сохраняем оценку локально
+            self.local_mdata = self.local_mdata.append({'date':date, 'ID':ID}, ignore_index=True)
+        
+        # Обновляем локальную таблицу
+        self.local_mdata.to_excel(LOCAL_MARK_DATA, index=False)
+            
+        
+    
+if __name__ == "__main__":
+    interlayer = Interlayer()
+    interlayer.put_mark("01.12.2021", 1)
+    interlayer.put_mark("01.12.2021", 2)
