6323bdf61b2e82476c2bad2f31e71b3835da0285
umarnurmatov <umarkhonnur12@gmail.com>
2022-02-18 19:31:54 +0300

    Глобальное обновление интерфеса
    Изменена структура интерфейса сайта;
    Добавлен ЛК
    Изменен дизайн
    Fixed bugs

--- visual.py
+++ visual.py
@@ -2,11 +2,13 @@ import os
 import shutil
 
 import cv2
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, Blueprint
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, exc
 import sqlite3 as sql
 
 from werkzeug.utils import secure_filename
+from werkzeug.security import check_password_hash, generate_password_hash
 from interlayer import main_func, start
 
 app = Flask(__name__)
@@ -14,12 +16,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 fr, s = start()
+user_id = None
+
+app.jinja_env.globals.update(teacher_name = None, lk_link = None)
 
 def get_connection(db_name):
     conn = sql.connect(db_name)
     cur = conn.cursor()
     return conn, cur
-
 
 def set_path(id):
     APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -46,6 +50,7 @@ class Teacher(db.Model):
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    cl = db.Column(db.Integer, nullable=False)
     teacher_id = db.Column(db.Integer)
 
     def __repr__(self):
@@ -57,7 +62,7 @@ def main():
     return render_template("main.html")
 
 
-@app.route("/to-do")
+@app.route("/todo")
 def instruction():
     return render_template("todo.html")
 
@@ -66,9 +71,16 @@ def instruction():
 def teacher_inf():
     return render_template("about.html")
 
+    
+@app.route('/error_no_access')
+def error_no_access():
+    return render_template('error_no_access.html')
+
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
+    global user_id
+    error = None
     if request.method == "POST":
 
         id = len(Teacher.query.order_by(Teacher.id).all())
@@ -76,53 +88,61 @@ def register():
         login = request.form['login']
         password = request.form['password']
 
-        t = Teacher(id=id, name=name, login=login, password=password)
+        if login == '' or name == '' or password == '':
+            error = 'Заполните все поля!'
 
-        try:
-            db.session.add(t)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'При регистрации произошла ошибка'
+        t = Teacher(id = id, name = name, login = login, password = generate_password_hash(password))
+        if error == None:
+            try:
+                db.session.add(t)
+                db.session.commit()
+                user_id = id
+                return redirect('/user=' + str(id) + '/lk')
+            except exc.IntegrityError:
+                error = f'Пользватель с логином "{login}" уже зарегестрирован!'
+            except:
+                return redirect('/error_register')
 
-    return render_template("register.html")
+    return render_template("register.html", error = error)
 
-
-user_id = None
-
-
-@app.route('/enter', methods=["POST", "GET"])
-def enter():
+@app.route('/login', methods=["POST", "GET"])
+def login():
     global user_id
     if request.method == "POST":
         conn, cur = get_connection('data.db')
+
         login = request.form['login']
         password = request.form['password']
-        ask = 'SELECT id FROM teacher WHERE login = "' + login + '" AND password = "' + password + '"'
-        print(ask)
-        ok = cur.execute(ask).fetchall()
-        try:
-            user_id = ok[0][0]
+
+        error = None
+        teacher = cur.execute('SELECT * FROM teacher WHERE login = ?', (login,)).fetchone()
+
+        if teacher == None:
+            error = 'Неверный логин.'
+        elif not check_password_hash(teacher[3], password):
+            error = 'Неверный пароль.'
+
+        if error == None:
+            user_id = teacher[0]
+            app.jinja_env.globals.update(teacher_name = teacher[1], lk_link = f'/user={teacher[0]}/lk')
             return redirect('/user=' + str(user_id) + '/lk')
-        except:
-            return 'При попытке входа произошла ошибка'
+        
+        return render_template("login.html", error = error)
+
+    return render_template("login.html", error = None)
 
 
-    return render_template("enter.html")
-
-
-@app.route('/user=<int:id>/lk', methods=["POST", "GET"])
+@app.route('/user=<int:id>/lk', methods = ["POST", "GET"])
 def lk(id):
     if id != user_id:
-        return "Нет доступа"
-    if request.method == "POST":
-        return redirect('/user=' + str(id) + '/put_mark')
+        return redirect('/error_no_access')
     conn, cur = get_connection('data.db')
-    ask = 'SELECT name, id FROM student WHERE teacher_id = ' + str(id)
+    ask = 'SELECT name, cl, id FROM student WHERE teacher_id = ' + str(id) + ' ORDER BY cl ASC'
     res = cur.execute(ask).fetchall()
     st = []
+    print(res)
     for i in res:
-        st.append([i[0], '/user=' + str(id) + '/delete_student/student_id=' + str(i[1])])
+        st.append([i[0], i[1], '/user=' + str(id) + '/delete_student/student_id=' + str(i[2])])
     ask = 'SELECT name FROM teacher WHERE id = ' + str(id)
     name = cur.execute(ask).fetchone()[0]
     print(name)
@@ -132,9 +152,10 @@ def lk(id):
 @app.route('/user=<int:id>/add_student', methods=["POST", "GET"])
 def add_student(id):
     if id != user_id:
-        return "Нет доступа"
+        return redirect('/error_no_access')
     if request.method == "POST":
         name = request.form['name']
+        cl = request.form['class']
         conn, cur = get_connection('data.db')
         ask = "SELECT COUNT(id) FROM student WHERE name = '" + name + "' AND teacher_id = " + str(id)
         inf = cur.execute(ask).fetchone()[0]
@@ -148,14 +169,13 @@ def add_student(id):
         photo2.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(photo2.filename)))
         photo3.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(photo3.filename)))
 
-        t = Student(id=inf, name=name, teacher_id=id)
+        t = Student(id=inf, name=name, cl = cl, teacher_id=id)
 
-        try:
-            db.session.add(t)
-            db.session.commit()
-            return redirect('/user=' + str(id) + '/lk')
-        except:
-            return 'При доабвлении ученика произошла ошибка'
+
+        db.session.add(t)
+        db.session.commit()
+        return redirect('/user=' + str(id) + '/lk')
+
 
     return render_template('add_student.html')
 
@@ -163,7 +183,7 @@ def add_student(id):
 @app.route('/user=<int:id>/delete_student/student_id=<int:st_id>', methods=["POST", "GET"])
 def delete_student(id, st_id):
     if id != user_id:
-        return 'Нет доступа'
+        return redirect('/error_no_access')
     if request.method == "POST":
         conn, cur = get_connection('data.db')
         ask = 'DELETE FROM student WHERE id = ' + str(st_id)
@@ -180,7 +200,7 @@ def delete_student(id, st_id):
 @app.route('/user=<int:id>/success_page/student_id=<int:st_id>')
 def success_page(id, st_id):
     if id != user_id:
-        return 'Нет доступа'
+        return redirect('/error_no_access')
     ask = "SELECT name FROM student WHERE id = " + str(st_id)
     conn, cur = get_connection('data.db')
     name = cur.execute(ask).fetchone()[0]
@@ -190,14 +210,14 @@ def success_page(id, st_id):
 @app.route('/user=<int:id>/error_page')
 def error_page(id):
     if id != user_id:
-        return 'Нет доступа'
+        return redirect('/error_no_access')
     return render_template('error_page.html', link="/user=" + str(id) + "/lk")
 
 
 @app.route('/user=<int:id>/put_mark', methods=['POST', 'GET'])
 def put_mark(id):
     if id != user_id:
-        return 'Нет доступа'
+        return redirect('/error_no_access')
     if request.method == 'POST':
         f = request.files['photo']
 
@@ -215,6 +235,10 @@ def put_mark(id):
         return redirect('/user=' + str(id) + '/success_page/student_id=' + str(face))
     return render_template('put_mark.html')
 
+@app.route('/singout')
+def singout():
+    app.jinja_env.globals.update(teacher_name = None, lk_link = None)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
