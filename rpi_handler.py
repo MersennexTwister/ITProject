5d455f559d91b0a15c9937dc7414602485b713eb
Semen-prog <82077476+Semen-prog@users.noreply.github.com>
2022-02-28 18:01:41 +0300

    upgrate button function

--- rpi_handler.py
+++ rpi_handler.py
@@ -1,10 +1,20 @@
 import requests as rqs
 import os, cv2
 import PIL.Image
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import time
+
+BUTTON = 8
+DELAY_SEC = 0.05
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+prev, cur = False, False
 
 URL = 'https://umarnurmatov.pythonanywhere.com/'
 LOGIN = 'testlogin44'
 PASSWORD = 'testpassword44'
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 def image_request():    
     video_capture = cv2.VideoCapture(0)
@@ -18,18 +28,27 @@ def image_request():
 
     img = PIL.Image.fromarray(rgb, 'RGB')
 
-    img.save('rpi_image_cache/cached.png')
-    img = open('rpi_image_cache/cached.png', 'rb')
+    img.save(APP_ROOT + '/rpi_image_cache/cached.png')
+    img = open(APP_ROOT + '/rpi_image_cache/cached.png', 'rb')
 
     files = {'photo': img}
     r = rqs.post(URL + 'lk/put_mark', files=files)
 
     img.close()
-    os.remove('rpi_image_cache/' + 'cached.png')
+    os.remove(APP_ROOT +'/rpi_image_cache/' + 'cached.png')
 
     print(r.text)
 
 if __name__ == '__main__':
-    image_request() 
+    while True: # Run forever
+        cur = GPIO.input(BUTTON)
+        if not prev and cur:
+            time.sleep(DELAY_SEC)
+            cur = GPIO.input(BUTTON)
+            if not prev and cur:
+                print("Button pressed")
+                image_request() 
+        prev = cur
+    
 
     
