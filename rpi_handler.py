fb8f6c279ce4a81cf34c9253e1c64aa378802a0f
umarnurmatov <umarkhonnur12@gmail.com>
2022-03-18 09:13:04 +0300

    Теперь rpi_handler еще и работает на raspberry

--- rpi_handler.py
+++ rpi_handler.py
@@ -1,51 +1,68 @@
 import requests as rqs
 import os
-# import RPi.GPIO as GPIO
-# import time
-# import picamera
-#
-# BUTTON = 8
-# DELAY_SEC = 0.05
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-# prev, cur = False, False
+import RPi.GPIO as GPIO
+import time
+import picamera
+
+BUTTON_P = 8
+BUTTON_M = 10
+DELAY_SEC = 0.05
+
+prev_p, cur_p = False, False
+prev_m, cur_m = False, False
 
 URL = 'https://umarnurmatov.pythonanywhere.com/'
 LOGIN = 'testlogin44'
 PASSWORD = 'testpassword44'
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# camera = picamera.PiCamera()
-# camera.resolution = (2160, 1440)
+camera = picamera.PiCamera()
 
+def setup():
+    global camera
+    camera.resolution = (2160, 1440)
 
-def image_request(s):
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(BUTTON_P, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(BUTTON_M, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+def image_request(mark):
     r = rqs.post(URL + 'login', data={'login': LOGIN, 'password': PASSWORD})
-
+    
     camera.capture(APP_ROOT + '/rpi_image_cache/cached.jpg')
-
+    
     img = open(APP_ROOT + '/rpi_image_cache/cached.jpg', 'rb')
 
     files = {'photo': img}
-    r = rqs.post(URL + 'lk/put_mark', files=files, data={"mark": s})
+
+    r = rqs.post(URL + 'lk/put_mark', files=files, data={'mark': mark})
 
     img.close()
-    os.remove(APP_ROOT + '/rpi_image_cache/' + 'cached.jpg')
+    os.remove(APP_ROOT +'/rpi_image_cache/' + 'cached.jpg')
 
     print(r.text)
 
 
 if __name__ == '__main__':
-    # while True:
-    #     cur = GPIO.input(BUTTON)
-    #     if not prev and cur:
-    #         time.sleep(DELAY_SEC)
-    #         cur = GPIO.input(BUTTON)
-    #         if not prev and cur:
-    #             print("Button pressed")
-    #             image_request()
-    #   prev = cur
+    setup()
+    
+    while True:
+        cur_p = GPIO.input(BUTTON_P)
+        cur_m = GPIO.input(BUTTON_M)
 
-    s = input("Какую оценку хотите выставить(+/-): ")
-    image_request(s)
+        if not prev_p and cur_p:
+            time.sleep(DELAY_SEC)
+            cur = GPIO.input(BUTTON_P)
+            if not prev_p and prev_p:
+                print("Button + pressed")
+                image_request('+') 
+        elif not prev_m and cur_m:
+            time.sleep(DELAY_SEC)
+            cur = GPIO.input(BUTTON_M)
+            if not prev_m and cur_m:
+                print("Button - pressed")
+                image_request('-') 
+
+        prev_p = cur_p
+        prev_m = cur_m
