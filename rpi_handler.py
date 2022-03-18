5249cf01295f58c3784eabe35dfd31d514eaa059
Semen-prog <82077476+Semen-prog@users.noreply.github.com>
2022-03-18 08:57:51 +0300

    Rpi-handler подкорректирован
    (теперь можно выставлять -))
    
    Функционал кнопки я не добавлял

--- rpi_handler.py
+++ rpi_handler.py
@@ -1,46 +1,51 @@
 import requests as rqs
 import os
-import RPi.GPIO as GPIO
-import time
-import picamera
-
-BUTTON = 8
-DELAY_SEC = 0.05
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-prev, cur = False, False
+# import RPi.GPIO as GPIO
+# import time
+# import picamera
+#
+# BUTTON = 8
+# DELAY_SEC = 0.05
+# GPIO.setwarnings(False)
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# prev, cur = False, False
 
 URL = 'https://umarnurmatov.pythonanywhere.com/'
 LOGIN = 'testlogin44'
 PASSWORD = 'testpassword44'
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-camera = picamera.PiCamera()
-camera.resolution = (2160, 1440)
+# camera = picamera.PiCamera()
+# camera.resolution = (2160, 1440)
 
-def image_request():
+
+def image_request(s):
     r = rqs.post(URL + 'login', data={'login': LOGIN, 'password': PASSWORD})
-    
+
     camera.capture(APP_ROOT + '/rpi_image_cache/cached.jpg')
-    
+
     img = open(APP_ROOT + '/rpi_image_cache/cached.jpg', 'rb')
 
     files = {'photo': img}
-    r = rqs.post(URL + 'lk/put_mark', files=files)
+    r = rqs.post(URL + 'lk/put_mark', files=files, data={"mark": s})
 
     img.close()
-    os.remove(APP_ROOT +'/rpi_image_cache/' + 'cached.jpg')
+    os.remove(APP_ROOT + '/rpi_image_cache/' + 'cached.jpg')
 
     print(r.text)
 
+
 if __name__ == '__main__':
-    while True:
-        cur = GPIO.input(BUTTON)
-        if not prev and cur:
-            time.sleep(DELAY_SEC)
-            cur = GPIO.input(BUTTON)
-            if not prev and cur:
-                print("Button pressed")
-                image_request() 
-        prev = cur
+    # while True:
+    #     cur = GPIO.input(BUTTON)
+    #     if not prev and cur:
+    #         time.sleep(DELAY_SEC)
+    #         cur = GPIO.input(BUTTON)
+    #         if not prev and cur:
+    #             print("Button pressed")
+    #             image_request()
+    #   prev = cur
+
+    s = input("Какую оценку хотите выставить(+/-): ")
+    image_request(s)
