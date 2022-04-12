2ceb41b2d16f96c9f9ebaa29476a937a91454d0d
umarnurmatov <umarkhonnur12@gmail.com>
2022-04-12 16:04:07 +0300

    Добавлено превью с камеры и полное выключение прог

--- rpi_handler.py
+++ rpi_handler.py
@@ -3,12 +3,10 @@ import os
 import RPi.GPIO as GPIO
 import time
 import picamera
-from tkinter import Tk, Label    
-from PIL import ImageTk, Image
-
 
 BUTTON_P = 8
 BUTTON_M = 10
+BUTTON_STOP = 12
 DELAY_SEC = 0.05
 
 prev_p, cur_p = False, False
@@ -21,10 +19,6 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 camera = picamera.PiCamera()
 
-window = Tk()
-window.title("project-mars")
-window.geometry("480x320")
-
 def setup():
     camera.resolution = (480, 320)
 
@@ -32,9 +26,9 @@ def setup():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(BUTTON_P, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setup(BUTTON_M, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(BUTTON_STOP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def image_request(mark):
-    global camera
     camera.resolution = (2160, 1440)
 
     r = rqs.post(URL + 'login', data={'login': LOGIN, 'password': PASSWORD})
@@ -53,36 +47,37 @@ def image_request(mark):
 
     print(r.text)
 
-def dispaly_frame():
-    global camera
-    camera.capture(APP_ROOT + '/rpi_image_cache/display_cache.jpg')
-    img = ImageTk.PhotoImage(APP_ROOT + "/rpi_image_cache/display_cache.jpg")
-    label = Label(window, image = img)
-    label.pack()
 
 
 if __name__ == '__main__':
     setup()
+
+    camera.start_preview()
     
     while True:
-        dispaly_frame()
-        window.mainloop()
-
         cur_p = GPIO.input(BUTTON_P)
         cur_m = GPIO.input(BUTTON_M)
+
+        if GPIO.input(BUTTON_STOP):
+            exit()
 
         if not prev_p and cur_p:
             time.sleep(DELAY_SEC)
             cur_p = GPIO.input(BUTTON_P)
             if not prev_p and cur_p:
+                camera.stop_preview()
                 print("Button + pressed")
-                image_request('+') 
+                image_request('+')
+                camera.start_preview()
+            
         elif not prev_m and cur_m:
             time.sleep(DELAY_SEC)
             cur_m = GPIO.input(BUTTON_M)
             if not prev_m and cur_m:
+                camera.stop_preview()
                 print("Button - pressed")
                 image_request('-') 
+                camera.start_preview()
 
         prev_p = cur_p
         prev_m = cur_m
