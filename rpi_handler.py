38165b267d5dfb8baca6e91b7afac6a2cc56c427
umarnurmatov <umarkhonnur12@gmail.com>
2022-04-12 13:53:00 +0300

    Добавление отображения в rpi

--- rpi_handler.py
+++ rpi_handler.py
@@ -3,6 +3,9 @@ import os
 import RPi.GPIO as GPIO
 import time
 import picamera
+from tkinter import Tk, Label    
+from PIL import ImageTk, Image
+
 
 BUTTON_P = 8
 BUTTON_M = 10
@@ -18,9 +21,12 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 camera = picamera.PiCamera()
 
+window = Tk()
+window.title("project-mars")
+window.geometry("480x320")
+
 def setup():
-    global camera
-    camera.resolution = (2160, 1440)
+    camera.resolution = (480, 320)
 
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
@@ -28,6 +34,9 @@ def setup():
     GPIO.setup(BUTTON_M, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def image_request(mark):
+    global camera
+    camera.resolution = (2160, 1440)
+
     r = rqs.post(URL + 'login', data={'login': LOGIN, 'password': PASSWORD})
     
     camera.capture(APP_ROOT + '/rpi_image_cache/cached.jpg')
@@ -40,14 +49,25 @@ def image_request(mark):
 
     img.close()
     os.remove(APP_ROOT +'/rpi_image_cache/' + 'cached.jpg')
+    camera.resolution = (480, 320)
 
     print(r.text)
+
+def dispaly_frame():
+    global camera
+    camera.capture(APP_ROOT + '/rpi_image_cache/display_cache.jpg')
+    img = ImageTk.PhotoImage(APP_ROOT + "/rpi_image_cache/display_cache.jpg")
+    label = Label(window, image = img)
+    label.pack()
 
 
 if __name__ == '__main__':
     setup()
     
     while True:
+        dispaly_frame()
+        window.mainloop()
+
         cur_p = GPIO.input(BUTTON_P)
         cur_m = GPIO.input(BUTTON_M)
 
