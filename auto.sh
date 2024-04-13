f8f7082e32d600e196af718d8459d8fc87537922
Semen Kukharenko <localhost@mail.ru>
2024-04-13 18:07:19 +0300

    Upd auto.sh

--- auto.sh
+++ auto.sh
@@ -24,16 +24,26 @@ echo Installing python requirements...
 
 wget -O /tmp/dlib.tar.gz "https://files.pythonhosted.org/packages/af/a4/226dbb659e913a4a149b35980e87e10050ea39a0dceac934e9e73cccbef1/dlib-19.24.4.tar.gz#sha256=f83dfdf6e85b91fc2b54ea5aad7932a5617cd61e9d13fc72055e22c7d9e264ff"
 tar -zxvf /tmp/dlib.tar.gz -C /tmp
+pip install cmake
 cd /tmp/dlib-19.24.4
 python3 /tmp/dlib-19.24.4/setup.py install
 pip install -r $PROJECT_DIR/requirements.txt
 
 echo Requirements installed successfully!
+echo Generating settings.py...
+
+cd $PROJECT_DIR
+touch settings.py
+export PWD=$( pwd )
+echo APP_ROOT=\"$PWD/\" > $PROJECT_DIR/settings.py
+echo SESSION_DUR=60 >> $PROJECT_DIR/settings.py
+
+echo Generation complete!
 echo Apache2 setup...
 
 touch /etc/apache2/sites-available/mars.conf
 cat $PROJECT_DIR/mars.conf > /etc/apache2/sites-available/mars.conf
-cat Listen 8000 >> /etc/apache2/ports.conf
+echo Listen 8000 >> /etc/apache2/ports.conf
 a2ensite mars
 systemctl restart apache2
 
