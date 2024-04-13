ffbfa96c7b8016af34a2fcf56622696ae23501fb
Semen Kukharenko <localhost@mail.ru>
2024-04-13 17:22:44 +0300

    Upd auto.sh

--- auto.sh
+++ auto.sh
@@ -9,10 +9,9 @@ a2enmod wsgi
 
 echo Clonning repo...
 
-useradd -m mars
 export PROJECT_DIR="/opt/project-mars"
 git clone https://github.com/MersennexTwister/project-mars $PROJECT_DIR
-chown -R mars:www-data $PROJECT_DIR
+chown -R :www-data $PROJECT_DIR
 chmod -R 775 $PROJECT_DIR
 
 echo Cloned successfully!
@@ -23,16 +22,18 @@ mkdir $PROJECT_DIR/encs $PROJECT_DIR/site_image_cache $PROJECT_DIR/static/undefi
 echo finished!
 echo Installing python requirements...
 
-sudo -u mars -s wget -O /home/mars/dlib.tar.gz "https://files.pythonhosted.org/packages/af/a4/226dbb659e913a4a149b35980e87e10050ea39a0dceac934e9e73cccbef1/dlib-19.24.4.tar.gz#sha256=f83dfdf6e85b91fc2b54ea5aad7932a5617cd61e9d13fc72055e22c7d9e264ff"
-sudo -u mars -s tar -zxvf dlib.tar.gz -C /home/mars
-sudo -u mars -s python3 /home/mars/dlib-19.24.4/setup.py install
-sudo -u mars -s pip install -r $PROJECT_DIR/requirements.txt
+wget -O /tmp/dlib.tar.gz "https://files.pythonhosted.org/packages/af/a4/226dbb659e913a4a149b35980e87e10050ea39a0dceac934e9e73cccbef1/dlib-19.24.4.tar.gz#sha256=f83dfdf6e85b91fc2b54ea5aad7932a5617cd61e9d13fc72055e22c7d9e264ff"
+tar -zxvf /tmp/dlib.tar.gz -C /tmp
+cd /tmp/dlib-19.24.4
+python3 /tmp/dlib-19.24.4/setup.py install
+pip install -r $PROJECT_DIR/requirements.txt
 
 echo Requirements installed successfully!
 echo Apache2 setup...
 
 touch /etc/apache2/sites-available/mars.conf
 cat $PROJECT_DIR/mars.conf > /etc/apache2/sites-available/mars.conf
+cat Listen 8000 >> /etc/apache2/ports.conf
 a2ensite mars
 systemctl restart apache2
 
